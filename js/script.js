34f7d3506bf7159a035c7bbf2dd1fdca9c5357d7
Ftoroplast <shpindler.ar@gmail.com>
2016-05-10 21:39:46 +0300

    Сделано 6 задание

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 var disabledElements = document.querySelectorAll(".disabled");
 var fields = document.querySelectorAll(".field");
 var placeholder = "";
+var btnContacts = document.querySelector("a.btn.btn--contacts");
+var contactsModalWindow = document.querySelector(".modal-window.modal-window--contacts");
+var overlay = document.querySelector(".overlay");
+var crossContacts = document.querySelector(".modal-window__cross.modal-window__cross--contacts");
 
 for (var i = 0; i < fields.length; ++i) {
   fields[i].addEventListener("focus", function () {
@@ -17,3 +21,29 @@ for (var i = 0; i < disabledElements.length; ++i) {
     event.preventDefault();
   });
 }
+
+btnContacts.addEventListener("click", function(event) {
+  event.preventDefault();
+  contactsModalWindow.classList.add("modal-window--show");
+  overlay.classList.add("overlay--show");
+});
+
+crossContacts.addEventListener("click", function(event) {
+  event.preventDefault();
+  contactsModalWindow.classList.remove("modal-window--show");
+  overlay.classList.remove("overlay--show");
+});
+
+overlay.addEventListener("click", function(event) {
+  contactsModalWindow.classList.remove("modal-window--show");
+  overlay.classList.remove("overlay--show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (contactsModalWindow.classList.contains("modal-window--show")) {
+      contactsModalWindow.classList.remove("modal-window--show");
+      overlay.classList.remove("overlay--show");
+    }
+  }
+});
