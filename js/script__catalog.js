64e605a029ce3e62f4ce0f9bcc2203b2363727a3
Ftoroplast <shpindler.ar@gmail.com>
2016-05-23 00:00:38 +0300

    Убран класс disabled

--- js/script__catalog.js
+++ js/script__catalog.js
@@ -1,11 +1,4 @@
-var disabledElements = document.querySelectorAll(".disabled");
 var fields = document.querySelectorAll(".form__field");
-
-for (var i = 0; i < disabledElements.length; ++i) {
-  disabledElements[i].addEventListener("click", function(event) {
-    event.preventDefault();
-  });
-}
 
 for (var i = 0; i < fields.length; ++i) {
   fields[i].addEventListener("focus", function () {
--- js/script__index.js
+++ js/script__index.js
@@ -1,4 +1,3 @@
-var disabledElements = document.querySelectorAll(".disabled");
 var fields = document.querySelectorAll(".form__field");
 var placeholder = "";
 var btnContacts = document.querySelector("a.btn.btn--contacts");
@@ -21,12 +20,6 @@ for (var i = 0; i < fields.length; ++i) {
   });
   fields[i].addEventListener("blur", function () {
     this.setAttribute("placeholder", placeholder);
-  });
-}
-
-for (var i = 0; i < disabledElements.length; ++i) {
-  disabledElements[i].addEventListener("click", function(event) {
-    event.preventDefault();
   });
 }
 
